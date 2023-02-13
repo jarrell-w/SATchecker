4ccc94b282c0406f9f86a0eb8468f173e62e5cda
jarrell-w <79674325+jarrell-w@users.noreply.github.com>
2023-02-12 20:34:26 -0500

    add read file within current directory functionality

--- satisfiabilitychecker/satisfiabilitychecker/src/satisfiabilitychecker/satisfiabilitychecker.java
+++ satisfiabilitychecker/satisfiabilitychecker/src/satisfiabilitychecker/satisfiabilitychecker.java
@@ -1,4 +1,17 @@
 package satisfiabilitychecker;
+import java.util.*;
+import java.io.*;
 
 public class satisfiabilitychecker {
-}
+    public static void main (String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the desired file name: ");
+        String fileName = scanner.next();
+        File file = new File(System.getProperty("user.dir") + "/src/satisfiabilitychecker/" + fileName + ".cnf");
+        Scanner sc = new Scanner(file);
+        while (sc.hasNextLine()) {
+            System.out.println(sc.nextLine());
+        }
+
+    }
+    }
