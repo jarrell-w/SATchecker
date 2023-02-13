b6347624db8f8e964b1c475fdd0d7a9fae2149b9
jarrell-w <79674325+jarrell-w@users.noreply.github.com>
2023-02-12 20:41:55 -0500

    change read file path

--- satisfiabilitychecker/satisfiabilitychecker/src/satisfiabilitychecker/satisfiabilitychecker.java
+++ satisfiabilitychecker/satisfiabilitychecker/src/satisfiabilitychecker/satisfiabilitychecker.java
@@ -7,7 +7,7 @@ public class satisfiabilitychecker {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the desired file name: ");
         String fileName = scanner.next();
-        File file = new File(System.getProperty("user.dir") + "/src/satisfiabilitychecker/" + fileName + ".cnf");
+        File file = new File(System.getProperty("user.dir") + "/" + fileName + ".cnf");
         Scanner sc = new Scanner(file);
         while (sc.hasNextLine()) {
             System.out.println(sc.nextLine());
