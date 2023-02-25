495ecc4603e09003b40912486ad7d6825579c620
jarrell-w <jarrellwitherspoon01@gmail.com>
2023-02-25 15:51:13 -0500

    add functionality for storing clauses

--- satisfiabilitychecker/satisfiabilitychecker/src/satisfiabilitychecker/satisfiabilitychecker.java
+++ satisfiabilitychecker/satisfiabilitychecker/src/satisfiabilitychecker/satisfiabilitychecker.java
@@ -11,18 +11,31 @@ public class satisfiabilitychecker {
         Scanner sc = new Scanner(file);
         int numOfVariables = 0;
         int numOfClauses = 0;
+        ArrayList<Integer> givenAssignments = new ArrayList<Integer>();
         while (sc.hasNextLine()) {
             String line = sc.nextLine();
-            if (line.charAt(0) != 'c') {
-                System.out.println(line);
-                if (line.charAt(0) == 'p') {
-                    numOfVariables = Character.getNumericValue(line.charAt(6));
-                    numOfClauses = Character.getNumericValue(line.charAt(8));
+            if (line.charAt(0) == 'p') {
+                numOfVariables = Character.getNumericValue(line.charAt(6));
+                numOfClauses = Character.getNumericValue(line.charAt(8));
+                while(sc.hasNext()) {
+                    try
+                    {
+                        int num = Integer.parseInt(sc.next());
+                        if (num != 0) {
+                            givenAssignments.add(num);
+                        }
+                    } catch (NumberFormatException ignored)
+                    {
+
+                    }
+
                 }
             }
-
         }
-        System.out.println(numOfClauses + numOfVariables);
+
+        int[] possibleAssignments = new int[numOfVariables];
+        Arrays.fill(possibleAssignments, 0);
+
 
     }
     public int setClauses(int numOfClauses, int numOfVariables) {
@@ -34,7 +47,7 @@ public class satisfiabilitychecker {
     * 0000 -> false false false false
     * 0001 -> false false false true
     * 0011 -> false false true true
-    * 0111 -> flase true true.
+    * 0111 -> flase true true true.
     * Start with all zeros and stop when there's all 1's in the amount of variables
     * We are adding 1 at a time.
     * When adding a 1, flip the right most zero to a 1, and everything after flip to a zero
@@ -47,7 +60,12 @@ public class satisfiabilitychecker {
     * 101
     * 110
     * 111
-    *
+    * Note 1: As long as there is one true in a clause it will always evaluate to true.
+    * Note 2: If any clause evaluates to false move to next, because it will always be false.
+    * Note 3: first step create a binary number of n(number of variables).
+    * Note 3: Map each index in the array to a variable. I.e [var1, var2, var3, var4]
+    * Note 4: Convert binary number in the array to true and false (array?)
+    * Note 5: Check clause to each number specified in file (if number is less than 1, multiple my -1 then check accordingly.
     * */
 }
 
