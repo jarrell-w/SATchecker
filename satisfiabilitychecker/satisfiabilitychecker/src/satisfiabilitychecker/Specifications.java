919953c685ceee4108500cd6a144095bc5ea3091
jarrell-w <jarrellwitherspoon01@gmail.com>
2023-02-27 13:54:28 -0500

    reduce code in main method, add Specifications class

---
+++ satisfiabilitychecker/satisfiabilitychecker/src/satisfiabilitychecker/Specifications.java
@@ -0,0 +1,59 @@
+package satisfiabilitychecker;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Specifications {
+    private int numOfVariables = 0;
+    private int numOfClauses = 0;
+    private int clauseLength = 0;
+    private final ArrayList<Integer> givenAssignments = new ArrayList<>();
+    private final ArrayList<Integer> listOfClauseLengths = new ArrayList<>();
+    public void setParams (String fileName) throws IOException {
+        File file = new File(System.getProperty("user.dir") + "/" + fileName + ".cnf");
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        Pattern pattern = Pattern.compile("-?\\d+");
+        String line;
+        while ((line = reader.readLine()) != null) {
+            if (line.startsWith("c")) {
+                continue;
+            } else if (line.startsWith("p")) {
+                String[] storeNums = line.split("\\s+");
+                this.numOfVariables = Integer.parseInt(storeNums[2]);
+                this.numOfClauses = Integer.parseInt(storeNums[3]);
+            } else {
+                Matcher matcher = pattern.matcher(line);
+                while (matcher.find()) {
+                    int num = Integer.parseInt(matcher.group());
+                    if (num != 0) {
+                        this.clauseLength++;
+                        this.givenAssignments.add(num);
+                    } else {
+                        this.listOfClauseLengths.add(clauseLength);
+                        this.clauseLength = 0;
+                    }
+                }
+            }
+        }
+        reader.close();
+    }
+
+    public int getNumOfVariables() {
+        return this.numOfVariables;
+    }
+    public int getNumOfClauses () {
+        return this.numOfClauses;
+    }
+
+    public ArrayList<Integer> getGivenAssignments() {
+        return givenAssignments;
+    }
+
+    public ArrayList<Integer> getListOfClauseLengths() {
+        return listOfClauseLengths;
+    }
+}
--- satisfiabilitychecker/satisfiabilitychecker/src/satisfiabilitychecker/satisfiabilitychecker.java
+++ satisfiabilitychecker/satisfiabilitychecker/src/satisfiabilitychecker/satisfiabilitychecker.java
@@ -1,53 +1,24 @@
 package satisfiabilitychecker;
 import java.util.*;
-import java.io.*;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class satisfiabilitychecker {
     public static void main (String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the desired file name: ");
         String fileName = scanner.next();
-        File file = new File(System.getProperty("user.dir") + "/" + fileName + ".cnf");
-        BufferedReader reader = new BufferedReader(new FileReader(file));
-        Pattern pattern = Pattern.compile("-?\\d+"); // regular expression to match integers
-        int numOfVariables = 0;
-        int numOfClauses = 0;
-        int clauseLength = 0;
-        String line;
-        //Functionality to store assignments and assignments.
-        ArrayList<Integer> givenAssignments = new ArrayList<>();
-        ArrayList<Integer> listOfClauseLengths = new ArrayList<>();
-        while ((line = reader.readLine()) != null) {
-            if (line.startsWith("c")) {
-                continue;
-            } else if (line.startsWith("p")) {
-                String[] storeNums = line.split("\\s+");
-                numOfVariables = Integer.parseInt(storeNums[2]);
-                numOfClauses = Integer.parseInt(storeNums[3]);
-            } else {
-                Matcher matcher = pattern.matcher(line);
-                while (matcher.find()) {
-                    int num = Integer.parseInt(matcher.group());
-                    if (num != 0) {
-                        clauseLength++;
-                        givenAssignments.add(num);
-                    } else {
-                        listOfClauseLengths.add(clauseLength);
-                        clauseLength = 0;
-                    }
-                }
-            }
-        }
-        reader.close();
+        Specifications getSpecs = new Specifications();
+        getSpecs.setParams(fileName);
 
-        System.out.println(givenAssignments);
+        int numOfVariables = getSpecs.getNumOfVariables();
+        int numOfClauses = getSpecs.getNumOfClauses();
         boolean assignmentNotSatisfied;
+
+        ArrayList<Integer> givenAssignments = getSpecs.getGivenAssignments();
+        ArrayList<Integer> listOfClauseLengths = getSpecs.getListOfClauseLengths();
+        System.out.println(givenAssignments);
+
         ArrayList<Integer> startingBinary = createBinaryOfN(numOfVariables);
         assignmentNotSatisfied = checkClauses(numOfClauses, convertBinaryToTrueOrFalse(startingBinary), givenAssignments, listOfClauseLengths);
         if (assignmentNotSatisfied) {
